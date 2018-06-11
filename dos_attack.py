b58c4e9a590bb32a5029ac3069d29e9b226e3b91
Leonel R <leorhcp2030@gmail.com>
2018-06-11 12:35:10 -0300

    Fixed. error de consepto en las listas del hash

--- dos_attack.py
+++ dos_attack.py
@@ -5,7 +5,7 @@
 def dos_attack(ips):
     """ devuelve una lista con los ips que realizaron un atake dos
 
-        pre: ips es un diccionario con key = ip, valor = estructura {direccion,tiempo,metodo,recurso}
+        pre: ips es un diccionario con key = ip, valor = [] que contiene [estructura] {direccion,tiempo,metodo,recurso}
         post: devueve una lista de ips = strings
     """
 
@@ -14,22 +14,41 @@ def dos_attack(ips):
 
     #interador (->c implements)
     #hay que eliminarlo cuando terminemos la implementacion
-    for ip in ips:
+    for ip_key in ips:
 
-        if ip.direccion in dos_dic:
-            tiempo = ip.tiempo
+        #iterador lista
+        for ip in ips[ip_key]:
 
-            if apariciones(tiempo, dos_dic[ip.direccion]) >= 5:
-                lista_tiempos.append(tiempo);
+            #no pertenece hash o !hash(key)
+            if ip.direccion not in dos_dic or dos_dic[ip.direccion] = []:
+                dos_dic[ip.direccion] = [ip]
+                continue
 
-                if ip.direccion not in dos_ip:
-                    dos_ip.append(ip.direccion)
+            dos_time_ultimo = dos_dic[ip.direccion][-1]
+
+            #no hay DOS
+            if dos_time_ultimo != ip.tiempo:
+                #limpio el registro
+                dos_dic[ip.direccion] = [];
 
             else:
-                #limpio los registros ya que los tiempos estan ordenados
-                dos_dic[ip.direccion] = []
+                #agrego al registro
+                dos_dic[ip.direccion].append(ip.tiempo)
+
+                #pregunto si el largo de la lista es mayor o igual 5
+                #en caso afirmativo tenemos un atake DOS
+                if len(dos_dic[ip.direccion]) >= 5:
+
+                    #verificamos si el IP esta en la BLACK LIST
+                    #si no esta la agregamos
+                    #en caso contrario no hacemos nada para evitar duplicados 
+                    if ip.direccion not in dos_ip:
+                        dos_ip.append(ip.direccion)
+
+
 
     #ya recorri todos los import ips
     #elimino DOS_DIC ( -> C )
     #elimino ITERADOR IPS ( -> C)
+    #elimino ITERADOR LISTA_IPS ( -> C)
     return dos_ip
