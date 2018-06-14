f67f4eb9675b9abaf8423f514e88cf7b6d0be9db
Leonel R <leorhcp2030@gmail.com>
2018-06-14 16:29:27 -0300

    Redefine ip_list -> time_list

--- dos_attack.c
+++ dos_attack.c
@@ -1,16 +1,16 @@
 /*
 	Chequea si una lista de tiempos de un Ip esta implementando un atacke DOS
 */
-bool dos_attack( lista_t* ip_list ){
+bool dos_attack( lista_t* time_list ){
 	
 	// si esta vacio o el largo de tiempos es menor a 5 no hay atacke
-	if ( lista_esta_vacia(ip_list) || lista_largo(ip_list) < 5 ) return false;
+	if ( lista_esta_vacia(time_list) || lista_largo(time_list) < 5 ) return false;
 	
-	lista_iter_t* primero = lista_iter_crear(ip_list);
+	lista_iter_t* primero = lista_iter_crear(time_list);
 	
 	if (!primero) return false;
 	
-	lista_iter_t* ultimo = lista_iter_crear(ip_list);
+	lista_iter_t* ultimo = lista_iter_crear(time_list);
 	
 	if (!ultimo){
 		lista_iter_destruir(primero);
@@ -22,7 +22,7 @@ bool dos_attack( lista_t* ip_list ){
 	//avanzo hasta la 5ta posicion 
 	for (int i=0; i < 5; i++) lista_iter_avanzar(ultimo);
 	
-	for ( int i = O; i <= lista_largo(ip_list) - 5; i++ ){
+	for ( int i = O; i <= lista_largo(time_list) - 5; i++ ){
 		void* tiempo1 = lista_iter_ver_actual(primero);
 		void* tiempo2 = lista_iter_ver_actual(ultimo);
 		
