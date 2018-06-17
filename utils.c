501ae7c62ff1a8b346ad6e610c6d999ab785e32a
Leonel R <leorhcp2030@gmail.com>
2018-06-17 01:33:13 -0300

    Update utils.c

--- utils.c
+++ utils.c
@@ -26,3 +26,15 @@ char* random_string(size_t tam){
 	
 	return cadena;
 }
+
+void volumen ( ){
+	bool ok = true
+	abb_t* arbol = abb_crear();
+	
+	for (int i = 0; i < 100000 ; i++){
+		ok &= abb_guardar( arbol, random_string(13) , random_string(3) );
+	}
+	print_test("se insertaron 100.000 elementos", ok);
+	
+	abb_destruir(arbol);
+}
