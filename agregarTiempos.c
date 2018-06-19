6c8c50e6a4a7a99a2e0ea3ac7a5648d9b501a832
Leonel R <leorhcp2030@gmail.com>
2018-06-19 02:30:18 -0300

    cargar archivo

--- agregarTiempos.c
+++ agregarTiempos.c
@@ -24,3 +24,44 @@ bool agregar_tiempos(hash_t* hash, const char* ip, time_t* tiempo){
 	
 return true;
 }
+
+
+bool tiempo_sospechoso( char* tiempo1, char* tiempo2 ){
+	time_t t1 = iso8601_to_time(tiempo1);
+	time_t t2 = iso8601_to_time(tiempo1);
+	return ( difftime(t1,t2) < 2 );
+}
+
+bool cargar_archivo(char* archivo){
+	hash_t* hash = hash_crear();
+	if (!hash) return false;
+	
+	FILE* f1;
+	
+	char* linea = NULL;
+	size_t capacidad = 0;
+	
+	
+	if((f1 = fopen(archivo,"r")) == NULL){
+		hash_destruir(hash);
+		return false;
+	}
+	
+	//cargo los tiempos al hash
+	for (int i= 0; getline(&linea, &capacidad, f1)>0; i++ ){
+		char** ip_tiempo_metodo_recurso = split(linea,'\t');
+		char* ip = ip_tiempo_metodo_recurso[0];
+		char* tiempo = ip_tiempo_metodo_recurso[1];
+		agregar_tiempos( hash, ip, tiempo);
+	}
+	
+	//verico los atackes
+	
+	DOS( hash ) ;
+	
+	free(linea);
+	fclose(f1);
+	
+	
+  }
+	
