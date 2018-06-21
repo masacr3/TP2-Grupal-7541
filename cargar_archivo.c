b5061a699d11c1b0225143e7ba3849adeb1c9a5f
Leonel R <leorhcp2030@gmail.com>
2018-06-21 00:42:26 -0300

    Create cargar_archivo.c

---
+++ cargar_archivo.c
@@ -0,0 +1,32 @@
+#C implementacion
+
+//wrapper copiar
+void copiar (char* destino, const char *cadena){
+	strcpy(destino, cadena);
+}
+
+//carga los tiempos del archivo al hash
+bool cargar_tiempos(char* archivo, hash_t* hash ){
+	FILE* f1;
+
+  	if((f1 = fopen(argv[1],"r")) == NULL) return false;
+	
+	char* linea_f1 = NULL; 
+	size_t capacidad_f1 = 0;
+	
+	for (int i= 0; getline(&linea, &capacidad, stdin)>0; i++ ){
+		char **datos = split(linea, '\t');
+		char* ip;
+		char* tiempo;
+		
+		//copio los tiempos
+		copiar(ip, linea[0]);
+		copiar(tiempo, linea[1]);
+		
+		guardar_tiempos(hash,ip,tiempo);
+		
+		free_strv(datos);
+		
+	}
+	return true;
+}
