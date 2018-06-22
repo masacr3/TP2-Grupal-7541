c23f6ab996cb1706d63b6760bfc6b3582e0f4389
Martín Stefanelli <38985104+martinstd96@users.noreply.github.com>
2018-06-22 13:15:11 -0300

    Modify cmp, cmp_tiempos and add cmp_ips and cmp_numeros

--- cmp.c
+++ cmp.c
@@ -10,14 +10,37 @@ int cmp_cadenas(const char* a, const char* b){
 	return res > 0 ? -1 : 1;
 }
 
+int cmp_numeros(const int a, const int b){
+	if (a < b) return -1;
+	
+        if (a > b) return 1;
+	
+        return 0;
+}
+
+int cmp_ips(char* ip1, char* ip2){
+	char** vector_ip1 = split(ip1, '.');
+	char** vector_ip2 = split(ip2, '.');
+	
+	for (int i = 0; i < 4; i++){
+		int resultado = cmp_numeros(atoi(vector_ip1[i]), atoi(vector_ip2[i]);
+		if (resultado > 0) return resultado;
+					    
+		if (resultado < 0) return resultado;
+					    
+	}
+					    
+	return 0;
+}
+
 int cmp_tiempos(const char* a, const char* b){
 	time_t t1 = iso8601_to_time(a);
 	time_t t2 = iso8601_to_time(b);
-	int res = difftime(t1,t2);
+	double res = difftime(t1,t2);
 	
-	if (res == 0) return 0;
+	if (res == 0.0) return 0;
 	
-	return res > 0 ? -1 : 1;
+	return res > 0.0 ? 1 : -1;
 }
 
 int cmp ( char** arr, char **arr2){
@@ -27,9 +50,11 @@ int cmp ( char** arr, char **arr2){
 	int res = cmp_tiempos(tiempo1,tiempo2);
 	
 	if ( res == 0 ){
-		int pos[] = {0,2,3}
-		for (int i=0; i<3; i++){
-			int resultado = cmp_cadenas( arr[ pos[i] ], arr[ pos[i] ]);
+		int pos[] = {0,3}
+		for (int i=0; i<2; i++){
+			int resultado;
+			if (i == 0) resultado = cmp_ips(arr[ pos[i] ],arr2[ pos[i] ]);
+			else resultado = cmp_cadenas( arr[ pos[i] ], arr2[ pos[i] ]);
 			if ( resultado != 0) return resultado;
 		}
 	}
