56911526dc97c9e881fea61acecbedecea47329c
Martín Stefanelli <38985104+martinstd96@users.noreply.github.com>
2018-06-25 00:23:14 -0300

    Add cmp_ad_hoc for merge files

--- cmp.c
+++ cmp.c
@@ -67,3 +67,8 @@ int cmp_registros(const void* a, const void* b){
 	return cmp(vector_a, vector_b);
 }
 	
+int cmp_ad_hoc(const void* a, const void* b){
+	char* registro_a = ad_hoc_obtener_registro(a);
+	char* registro_b = ad_hoc_obtener_registro(b);
+	return cmp_registros(registro_a, registro_b);
+}
