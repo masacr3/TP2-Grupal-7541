b03b0c05284b99e45a10942ced9feede816f8a98
Leonel R <leorhcp2030@gmail.com>
2018-07-01 01:58:51 -0300

    3 tipos de atake

--- DOS/pruebas.c
+++ DOS/pruebas.c
@@ -7,13 +7,33 @@
 #include <unistd.h>  // For ssize_t in Linux.
 
 
-void pruebas_DOS (){
+void pruebas_DOS_atake (){
 
   hash_t* hash = hash_crear(NULL);
   heap_t heap = heap_crear(strcmp);
 
   char* lista_ip[] = {"1","1","1","1","1"};
   char* tiempos[] = {"10","10","10","10","10"};
+
+  for (int i=0; i<5; i++) hash_guardar(hast, lista_ip[i], tiempos[i]);
+
+  DOS(hash,heap);
+
+  fprintf(stdout, "%s\n", heap_esta_vacio(heap) ? "No hay ataque" : "Hay ataque D.O.S" );
+
+  hash_destruir(hash,NULL);
+  heap_destruir(heap,NULL);
+
+
+}
+
+void pruebas_DOS_Noatake (){
+
+  hash_t* hash = hash_crear(NULL);
+  heap_t heap = heap_crear(strcmp);
+
+  char* lista_ip[] = {"1","1","1","1"};
+  char* tiempos[] = {"10","10","10","10"};
 
   for (int i=0; i<4; i++) hash_guardar(hast, lista_ip[i], tiempos[i]);
 
@@ -24,5 +44,31 @@ void pruebas_DOS (){
   hash_destruir(hash,NULL);
   heap_destruir(heap,NULL);
 
+
+}
+
+void pruebas_DOS (){
+
+  hash_t* hash = hash_crear(NULL);
+  heap_t heap = heap_crear(strcmp);
+
+  char* lista_ip[] = {"1","1","1","1","1","11","11","11","11","11","2","2","2","2","2"};
+  char* tiempos[] = {"10","10","10","10","10","11","11","11","11","11","2","2","2","2","2"};
+
+  for (int i=0; i<15; i++) hash_guardar(hast, lista_ip[i], tiempos[i]);
+
+  DOS(hash,heap);
+
+  fprintf(stdout, "%s\n", heap_esta_vacio(heap) ? "No hay ataque" : "Hay ataque D.O.S" );
+
+  hash_destruir(hash,NULL);
+  heap_destruir(heap,NULL);
+
+
+}
+
+void pruebas (){
+  pruebas_DOS_atake();
+  pruebas_DOS_Noatake();
 
 }
